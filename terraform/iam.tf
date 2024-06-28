e1eae2d9b1a3c283894627773653f9a6bf5a42e3
Alves0611 <g.alves0611@gmail.com>
2024-06-28 12:32:52 -0300

    feat: add IAM role and OIDC provider for GitHub Actions
    - Create aws_iam_openid_connect_provider for GitHub Actions
    - Define aws_iam_role with assume role policy for GitHub Actions

---
+++ terraform/iam.tf
@@ -0,0 +1,33 @@
+resource "aws_iam_openid_connect_provider" "this" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["1b511abead59c6ce207077c0bf0e0043b1382612"]
+}
+
+
+resource "aws_iam_role" "test_role" {
+  name = "github-actions-ecr-role"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Principal" : {
+          "Federated" : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
+        },
+        "Condition" : {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:aud" : [
+              "sts.amazonaws.com"
+            ],
+            "token.actions.githubusercontent.com:sub" : [
+              "repo:${var.repository}:ref:refs/heads/main"
+            ]
+          }
+        }
+      }
+    ]
+  })
+}
